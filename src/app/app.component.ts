f2a6268ae3d0755bb02a95708ca7acf2ef48d612
¨dannygopi¨ <gp.danny@gmail.com>
2018-02-11 19:18:16 -0500

    Initial app configuration

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Praticas Angular';
+}
